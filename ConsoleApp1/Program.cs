37753e258931613291d667985f8b3169d31f55f9
BadFatRat <61916463+BadFatRat@users.noreply.github.com>
2020-09-11 21:02:35 +0300

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -22,17 +22,17 @@ namespace ConsoleApp1
             r2 = CheckNumber(R2);
             if (r1 <= r2)
             {
-                Console.WriteLine("R1 должен быть больше чем R2");
+                Console.WriteLine("R1 должен быть больше чем R2.");
                 Environment.Exit(0);
             }
             S1 = pi * (float)Math.Pow(r1, 2);
             S2 = pi * (float)Math.Pow(r2, 2);
             S3 = S1 - S2;
-            Console.WriteLine("Площадь первого круга: S1 = {0}", Math.Round(S1, 3));
-            Console.WriteLine("Площадь второго круга: S2 = {0}", Math.Round(S2, 3));
-            Console.WriteLine("Площадь кольца, внешний радиус которого равен R1, а внутренний радиус равен R2: S3 = {0}", Math.Round(S3, 3));
+            Console.WriteLine("Площадь первого круга: S1 = {0}.", Math.Round(S1, 3));
+            Console.WriteLine("Площадь второго круга: S2 = {0}.", Math.Round(S2, 3));
+            Console.WriteLine("Площадь кольца, внешний радиус которого равен R1, а внутренний радиус равен R2: S3 = {0}.", Math.Round(S3, 3));
         }
-        static float CheckNumber(string user_input) // проверяет, является ли введённое значение числом типа double
+        static float CheckNumber(string user_input) // проверяет, является ли введённое значение числом типа float
         {
             if (float.TryParse(user_input, out float value))
             {
